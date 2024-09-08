a7a1ca8dc4cbf130e79fc0f53ec06ea020cccfe5
Odjoumani Tayo Déo-Gracias Giscar-Bonaventure <deogracias.odjoumani@gmail.com>
2024-09-07 20:37:34 -0400

    Route for quizPage added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import ConnexionPage from './pages/connexionPage';
 import WelcomePage from './pages/welcomePage';
+import QuizPage from './pages/quizPage';
 
 
 export default function App(){
@@ -17,6 +18,7 @@ export default function App(){
         <Routes>
           <Route exact path='/' element = {<WelcomePage/>}/>
           <Route path='/connexionPage' element = {<ConnexionPage/>}/>
+          <Route path='/quizPage' element = {<QuizPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
